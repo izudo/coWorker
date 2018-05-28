747a2b734fafd55b197b1e0c51fd77700dec9bfd
johan <youhan.pan@mindertech.co.uk>
2018-05-28 17:19:32 +0800

    aa

---
+++ worker/CoWorker.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weirdo
+ * Date: 2018/5/28
+ * Time: 17:03
+ */
+
+namespace Worker;
+
+
+use Workerman\Worker;
+
+class CoWorker extends Worker
+{
+
+}
